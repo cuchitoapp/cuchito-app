0794779976d98dfa70571b8e1078155b50b888eb
KTComar <marco16@egmail.com>
2020-09-03 19:45:58 -0400

    14

--- lib/screens/UploadPage.dart
+++ lib/screens/UploadPage.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadPage extends StatefulWidget {
   @override
@@ -6,8 +8,96 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+  File file;
+  captureImageWithCamera() async {
+    Navigator.pop(context);
+    File imageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      maxHeight: 680,
+      maxWidth: 970,
+    );
+    setState(() {
+      this.file = imageFile;
+    });
+  }
+
+  pickImageFromGallery() async {
+    Navigator.pop(context);
+    File imageFile = await ImagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
+    setState(() {
+      this.file = imageFile;
+    });
+  }
+
+  takeImage(mContext) {
+    return showDialog(
+      context: mContext,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text(
+            'Nuevo Post',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          children: [
+            SimpleDialogOption(
+              child: Text(
+                'Capturar imagen con la camara',
+                style: TextStyle(color: Colors.black),
+              ),
+              onPressed: captureImageWithCamera,
+            ),
+            SimpleDialogOption(
+              child: Text(
+                'Seleccionar imagen de la galeria',
+                style: TextStyle(color: Colors.black),
+              ),
+              onPressed: pickImageFromGallery,
+            ),
+            SimpleDialogOption(
+              child: Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.black),
+              ),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  displayUploadScreen() {
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.add_photo_alternate,
+            color: Colors.grey,
+            size: 200.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9.0)),
+              child: Text(
+                'Subir Imagen',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+              onPressed: () => takeImage(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return displayUploadScreen();
   }
 }
