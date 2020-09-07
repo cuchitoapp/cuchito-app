a22ca7857463bd462f191d78455703e132f6dbb5
KTComar <marco16@egmail.com>
2020-09-07 01:32:21 -0300

    17

--- lib/screens/ProfilePage.dart
+++ lib/screens/ProfilePage.dart
@@ -1,8 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cuchitoapp/models/user.dart';
 import 'package:cuchitoapp/registro/login.dart';
 import 'package:cuchitoapp/screens/EditProfilePage.dart';
 import 'package:cuchitoapp/widgets/HeaderWidget.dart';
+import 'package:cuchitoapp/widgets/PostTileWidget.dart';
+import 'package:cuchitoapp/widgets/PostWidget.dart';
 import 'package:cuchitoapp/widgets/ProgressWidget.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +18,15 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   final String currentOnlineUserId = currentUser?.id;
+  bool loading = false;
+  int countPost = 0;
+  List<Post> postsList = [];
+  String postOrientation = "grid";
+
+  void initState() {
+    getAllProfilePosts();
+  }
+
   createProfileTopView() {
     return FutureBuilder(
       future: usersReference.document(widget.userProfileId).get(),
@@ -164,8 +176,110 @@ class _ProfilePageState extends State<ProfilePage> {
       body: ListView(
         children: [
           createProfileTopView(),
+          Divider(),
+          createListAndGridPostOrientation(),
+          Divider(
+            height: 0.0,
+          ),
+          displayProfilePost(),
         ],
       ),
     );
+  }
+
+  displayProfilePost() {
+    if (loading) {
+      return circularProgress();
+    } else if (postsList.isEmpty) {
+      return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Icon(
+                Icons.photo_library,
+                color: Colors.grey,
+                size: 200.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Text(
+                "No Posts",
+                style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else if (postOrientation == "grid") {
+      List<GridTile> gridTilesList = [];
+      postsList.forEach((eachPost) {
+        gridTilesList.add(GridTile(child: PostTile(eachPost)));
+      });
+      return GridView.count(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 1.5,
+        crossAxisSpacing: 1.5,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        children: gridTilesList,
+      );
+    } else if (postOrientation == "list") {
+      return Column(
+        children: postsList,
+      );
+    }
+  }
+
+  getAllProfilePosts() async {
+    setState(() {
+      loading = true;
+    });
+    QuerySnapshot querySnapshot = await postsReference
+        .document(widget.userProfileId)
+        .collection("usersPosts")
+        .orderBy("timestamp", descending: true)
+        .getDocuments();
+    setState(() {
+      loading = false;
+      countPost = querySnapshot.documents.length;
+      postsList = querySnapshot.documents
+          .map((documentSnapshot) => Post.fromDocument(documentSnapshot))
+          .toList();
+    });
+  }
+
+  createListAndGridPostOrientation() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        IconButton(
+          onPressed: () => setOrientation("grid"),
+          icon: Icon(Icons.grid_on),
+          color: postOrientation == "grid"
+              ? Theme.of(context).primaryColor
+              : Colors.grey,
+        ),
+        IconButton(
+          onPressed: () => setOrientation("list"),
+          icon: Icon(Icons.list),
+          color: postOrientation == "list"
+              ? Theme.of(context).primaryColor
+              : Colors.grey,
+        ),
+      ],
+    );
+  }
+
+  setOrientation(String orientation) {
+    setState(() {
+      this.postOrientation = orientation;
+    });
   }
 }
---
+++ lib/widgets/PostWidget.dart
@@ -0,0 +1,222 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cuchitoapp/models/user.dart';
+import 'package:cuchitoapp/registro/login.dart';
+import 'package:cuchitoapp/widgets/ProgressWidget.dart';
+import 'package:flutter/material.dart';
+
+class Post extends StatefulWidget {
+  final String postId;
+  final String ownerId;
+  //final String timestamp;
+  final dynamic likes;
+  final String username;
+  final String description;
+  final String location;
+  final String url;
+
+  Post({
+    this.postId,
+    this.ownerId,
+    //this.timestamp,
+    this.likes,
+    this.username,
+    this.description,
+    this.location,
+    this.url,
+  });
+
+  factory Post.fromDocument(DocumentSnapshot documentSnapshot) {
+    return Post(
+      postId: documentSnapshot["postId"],
+      ownerId: documentSnapshot["ownerId"],
+      //timestamp: documentSnapshot["postId"],
+      likes: documentSnapshot["likes"],
+      username: documentSnapshot["username"],
+      description: documentSnapshot["description"],
+      location: documentSnapshot["location"],
+      url: documentSnapshot["url"],
+    );
+  }
+  int getTotalNumberOfLikes(likes) {
+    if (likes == null) {
+      return 0;
+    }
+    int counter = 0;
+    likes.values.forEach((eachValue) {
+      if (eachValue == true) {
+        counter++;
+      }
+    });
+    return counter;
+  }
+
+  @override
+  _PostState createState() => _PostState(
+        postId: this.postId,
+        ownerId: this.ownerId,
+        //timestamp: this.timestamp,
+        likes: this.likes,
+        username: this.username,
+        description: this.description,
+        location: this.location,
+        url: this.url,
+        likeCount: getTotalNumberOfLikes(this.likes),
+      );
+}
+
+class _PostState extends State<Post> {
+  final String postId;
+  final String ownerId;
+  //final String timestamp;
+  Map likes;
+  final String username;
+  final String description;
+  final String location;
+  final String url;
+  int likeCount;
+  bool isLiked;
+  bool showHeart = false;
+  final String currentOnlineUserId = currentUser?.id;
+
+  _PostState({
+    this.postId,
+    this.ownerId,
+    //this.timestamp,
+    this.likes,
+    this.username,
+    this.description,
+    this.location,
+    this.url,
+    this.likeCount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 12.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          createPostHead(),
+          createPostPicture(),
+          createPostFooter(),
+        ],
+      ),
+    );
+  }
+
+  createPostHead() {
+    return FutureBuilder(
+      future: usersReference.document(ownerId).get(),
+      builder: (context, dataSnapshot) {
+        if (!dataSnapshot.hasData) {
+          return circularProgress();
+        }
+        User user = User.fromDocument(dataSnapshot.data);
+        bool isPostOwner = currentOnlineUserId == ownerId;
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: CachedNetworkImageProvider(user.url),
+            backgroundColor: Colors.grey,
+          ),
+          title: GestureDetector(
+            onTap: () => print("show profile"),
+            child: Text(
+              user.username,
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+          ),
+          subtitle: Text(
+            location,
+            style: TextStyle(color: Colors.black),
+          ),
+          trailing: isPostOwner
+              ? IconButton(
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Colors.black,
+                  ),
+                  onPressed: () => print("deleted"),
+                )
+              : Text(""),
+        );
+      },
+    );
+  }
+
+  createPostPicture() {
+    return GestureDetector(
+      onDoubleTap: () => print("post Liked"),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Image.network(url),
+        ],
+      ),
+    );
+  }
+
+  createPostFooter() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(padding: EdgeInsets.only(top: 40.0, left: 20.0)),
+            GestureDetector(
+              onTap: () => print("liked post"),
+              child: Icon(
+                Icons.favorite, color: Colors.green,
+                //isLiked ? Icons.favorite : Icons.favorite_border,
+                //size: 28.0,
+                //color: Colors.pink,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(right: 20.0)),
+            GestureDetector(
+              onTap: () => print("show coments"),
+              child: Icon(
+                Icons.chat_bubble_outline,
+                size: 28.0,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$likeCount likes",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$username ",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                description,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
